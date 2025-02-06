b5695aeb75f35f57ccc25868b536e75bc3287029
corcheesey <micahcortese@gmail.com>
2025-02-05 20:32:32 -0500

    Added the script tag to its proper position at the bottom of the body tags. Renamed the textarea id and removed the span inside of the <p> tag. Added in styling for when the char limit is reached. Main.js creates an event listener that takes the value of the length of the textarea and adds the new limit styling to the border and char counter if the if / else conditions are met.

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-var wordCount = document.getElementById("counter");
+const charCount = document.getElementById("wordcount");
+const textArea = document.getElementById("userinput");
+const maxChars = 250;
 
-console.log(wordCount);
+textArea.addEventListener("input", function() {
+    const currentLength = textArea.value.length;
+    charCount.textContent = `${currentLength} / ${maxChars}`;
+
+    if (currentLength >= maxChars) {
+        textArea.classList.add("limit");
+        charCount.classList.add("limit");
+    } else {
+        textArea.classList.remove("limit");
+        charCount.classList.remove("limit");
+    }
+});
